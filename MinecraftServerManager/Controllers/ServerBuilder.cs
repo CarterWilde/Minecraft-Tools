25aea45bb9d631e734f60e27be42afeee6d0f6d5
CarterWilde <carterjwilde@gmail.com>
2019-02-21 19:20:16 -0700

    Cleaned up some methods.

--- MinecraftServerManager/Controllers/ServerBuilder.cs
+++ MinecraftServerManager/Controllers/ServerBuilder.cs
@@ -11,28 +11,25 @@ namespace MinecraftServerInstaller.Controllers {
   public class ServerBuilder {
     public ServerConfig Config { get; private set; }
 
-    private IList<Thread> ServerBuilderThreads;
-
     //Creates a ServerBuilder with a ServerConfig and makes it the current instance
     public ServerBuilder(ServerConfig config) {
       Config = config;
-      ServerBuilderThreads = new List<Thread>();
     }
 
     //Creates a ServerBuilder with a config file path to make a ServerConfig with
     //that files, and stores the ServerConfig from that.
     public ServerBuilder(string ConfigFilePath) {
       Config = DeserializeConfig(ConfigFilePath);
-      ServerBuilderThreads = new List<Thread>();
     }
 
     //Takes nothing and builds from the protected config
     public void BuildAll() {
+      IList<Thread> ServerBuilderThreads = new List<Thread>();
       foreach (Server server in Config.Servers) {
         Thread thread = new Thread(() => {
           Build(server);
         }) {
-          Name = $"Build Thread[{server.Name}]"
+          Name = $"BuildThread[{server.Name}]"
         };
         Console.WriteLine("Starting " + thread.Name);
         ServerBuilderThreads.Add(thread);
@@ -50,16 +47,16 @@ namespace MinecraftServerInstaller.Controllers {
       Directory.CreateDirectory(Config.ConfigMeta.Path + "/" + server.Name);
       Directory.CreateDirectory(Config.ConfigMeta.BackupsPath + "/" + server.Name);
       Directory.CreateDirectory(Config.ConfigMeta.Path + "/" + server.Name + $"/ManagerLog");
-      DownloadServerBinarys(server, Config.ConfigMeta);
-      CreateEULA(server, Config.ConfigMeta);
-      BuildServerProperties(server, Config.ConfigMeta);
-      Console.WriteLine("Done building server " + server.Name);
+      DownloadServerBinarys(server);
+      CreateEULA(server);
+      BuildServerProperties(server);
+      Console.WriteLine("Done building server {0}", server.Name);
     }
 
     //Create the server.properties file
-    public static void BuildServerProperties(Server server, ServerConfigMeta meta) {
+    public void BuildServerProperties(Server server) {
       Console.WriteLine("Creating server.properties for server {0}", server.Name);
-      using (FileStream stream = new FileStream(meta.Path + "/" + server.Name + "/server.properties", FileMode.Create, FileAccess.ReadWrite)) {
+      using (FileStream stream = new FileStream(Config.ConfigMeta.Path + "/" + server.Name + "/server.properties", FileMode.Create, FileAccess.ReadWrite)) {
         using (StreamWriter writer = new StreamWriter(stream)) {
           Parallel.ForEach(server.Properties.Keys, (key) => {
             writer.WriteLine(ToPropertiesString(key, server.Properties[key]));
@@ -73,19 +70,19 @@ namespace MinecraftServerInstaller.Controllers {
     }
 
     //Create EULA file and set it to true by default
-    public static void CreateEULA(Server server, ServerConfigMeta meta) {
-      using (FileStream stream = new FileStream(meta.Path + "/" + server.Name + "/eula.txt", FileMode.Create, FileAccess.ReadWrite)) {
+    public void CreateEULA(Server server) {
+      using (FileStream stream = new FileStream(Config.ConfigMeta.Path + "/" + server.Name + "/eula.txt", FileMode.Create, FileAccess.ReadWrite)) {
         using (StreamWriter writer = new StreamWriter(stream)) {
-          writer.WriteAsync(meta.EULA);
+          writer.WriteAsync(Config.ConfigMeta.EULA);
         }
       }
     }
 
     //Download binarys for server
-    public static void DownloadServerBinarys(Server server, ServerConfigMeta meta) {
+    public void DownloadServerBinarys(Server server) {
       using (WebClient client = new WebClient()) {
         Console.WriteLine($"Downloading server binarys for {server.Name}...");
-        client.DownloadFile(new Uri(server.MinecraftServerUrl), $"{meta.Path}/{server.Name}/[{server.GameVersion}]server.jar");
+        client.DownloadFile(new Uri(server.MinecraftServerUrl), $"{Config.ConfigMeta.Path}/{server.Name}/[{server.GameVersion}]server.jar");
         Console.WriteLine($"Finished downloading server binarys for {server.Name}!");
       }
     }

b75cc6558d9d511688bfc6877c940c2b336134da
carterjwilde <carterjwilde@gmail.com>
2019-11-24 16:55:03 -0700

    Updated Client

--- MinecraftClientInstaller/Program.cs
+++ MinecraftClientInstaller/Program.cs
@@ -1,8 +1,7 @@
 ﻿using MinecraftClientInstaller.Model;
-using Newtonsoft.Json;
+using System.Text.Json;
 using System;
 using System.IO;
-using System.Runtime.Serialization.Json;
 
 namespace MinecraftClientInstaller{
   class Program {
@@ -20,14 +19,14 @@ namespace MinecraftClientInstaller{
       DownloadManager manager = new DownloadManager(ModpackPath, modpack.Meta.GameVersion);
       Console.WriteLine("Downloading to " + ModpackPath);
       Console.WriteLine("Starting Downloads! Some of these mods might be large, so it could be awhile.");
-      manager.DownloadMods(modpack.Joint);
-      manager.DownloadMods(modpack.Client);
+      manager.DownloadMods(modpack?.Joint);
+      manager.DownloadMods(modpack?.Client);
     }
 
     static Modpack DeserializeModpack(string FileName) {
       using (FileStream package = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite)) {
         using (StreamReader stream = new StreamReader(package)) {
-          return JsonConvert.DeserializeObject<Modpack>(stream.ReadToEnd());
+          return JsonSerializer.Deserialize<Modpack>(stream.ReadToEnd());
         }
       }
     }
